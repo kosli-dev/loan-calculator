5a2301beec69d6aceb083dbc479153c97fe02978
Mike Long <mike@compliancedb.com>
2021-02-05 11:35:47 +0100

    Really fix security issue

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 def load_unsafe_yaml():
     import yaml
     # Uncomment this line to create a security issue - unsafe yaml load
-    yaml.load("yaml_str")
+    # yaml.load("yaml_str")
 
 def main():
     print("Loan calculator")
