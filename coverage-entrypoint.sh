e96bb1c0441f8311c60fd70938b9157860d3a537
Jon Jagger <jon@jaggersoft.com>
2021-02-16 09:16:47 +0000

    Don't create unused coverage_summary.json in coverage entrypoint

--- coverage-entrypoint.sh
+++ coverage-entrypoint.sh
@@ -5,22 +5,20 @@ set -e
 
 OUTDIR=build/coverage
 
-#coverage html -d $OUTDIR
-pytest --cov-report html:$OUTDIR/html \
-       --cov-report xml:$OUTDIR/cov.xml \
+pytest --cov-report html:${OUTDIR}/html \
+       --cov-report xml:${OUTDIR}/cov.xml \
        --cov-report term \
        --cov=src \
-    | tee $OUTDIR/coverage_output.txt
+    | tee ${OUTDIR}/coverage_output.txt
 
 # Create a file containing the coverage percentage
-cat $OUTDIR/coverage_output.txt | grep TOTAL | awk '{print "COVERAGE=\""$4"\""}' > $OUTDIR/test_coverage.sh
+cat ${OUTDIR}/coverage_output.txt | grep TOTAL | awk '{print "COVERAGE=\""$4"\""}' > ${OUTDIR}/test_coverage.sh
 
 # Create a file containing the number of test cases
 TEST_CASES=`pytest --collect-only -q  --ignore=integration_tests | head -n -2 | wc -l`
-echo TEST_CASES=$TEST_CASES > $OUTDIR/test_cases.sh
+echo TEST_CASES=${TEST_CASES} > ${OUTDIR}/test_cases.sh
 
 source $OUTDIR/test_coverage.sh
 source $OUTDIR/test_cases.sh
 
-echo "{\"test_cases\": $TEST_CASES, \"coverage\": \"$COVERAGE\"}" > $OUTDIR/coverage_summary.json
-echo COVERAGE_SUMMARY="\"$TEST_CASES tests passed with $COVERAGE coverage\"" > $OUTDIR/coverage_summary.sh
+echo COVERAGE_SUMMARY="\"${TEST_CASES} tests passed with ${COVERAGE} coverage\"" > ${OUTDIR}/coverage_summary.sh
