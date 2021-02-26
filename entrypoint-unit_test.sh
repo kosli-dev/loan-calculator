b5fd511db0b32752fab1d2da152f990640bef396
Jon Jagger <jon@jaggersoft.com>
2021-02-26 07:15:38 +0000

    Inline 'make test' into .github/workflows/master_pipeline.yml

--- entrypoint-unit_test.sh
+++ entrypoint-unit_test.sh
@@ -4,4 +4,4 @@
 set -e
 rm -rf build/test
 mkdir -p build/test
-pytest -rA --ignore=integration_tests --capture=no --junit-xml=build/test/pytest_unit.xml
+pytest -rA --capture=no --junit-xml=build/test/pytest_unit.xml
